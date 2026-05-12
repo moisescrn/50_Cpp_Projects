4b4a9ebf4ce6101fa34f6f37fecfb67b9624c732
moisescrn <moisescarbofuster@hotmail.com>
2026-05-12 07:42:13 +0200

    Completion of 02

--- 01_CGPA_Calculator/main.cpp
+++ 01_CGPA_Calculator/main.cpp
@@ -5,29 +5,50 @@ The weight (credits) of each subject may be given from 0 to 10
 */
 
 #include <iostream>
-#include <cstdarg>    // Contains va_list, va_start, ...
+using namespace std;
 
-double cgpa(int n_args, ...){
+// Let's work with pointers, since it has more flexibility than arrays
+
+double cgpa(int n_subj, int* grades, int* weights){
     // First argument is the number of subjects
-    // The rest of arguments shall be "grade and weight" alternantingly
+    // The second is the address of where the grades begin
+    // The third is the address of where the weights begin
+
+    // prepare enough memory for each "array" of numbers
+    //grades = (int*)malloc(sizeof(int)*n_subj);
+    //weights = (int*)malloc(sizeof(int)*n_subj);
+    // for the flow of this program we have to prepare the memory before the input
+    // but in a more general case I think we would need this here
 
     double sum = 0.0; // double to enable a double result at the end
+    int total_weights = 0;
 
-    // Read the arguments
-    va_list args;
-    va_start(args,n_args);
-
-    for (int i = 0; i < n_args; i++){
-        sum += va_arg(args, int) * va_arg(args,int); // Grade*Weight
+    for (int i = 0; i < n_subj; i++){
+        sum += *(grades+i) * *(weights+i);
+        total_weights += *(weights+i);
     }
-    va_end(args);
 
-    double mean = sum / n_args;
-
-    return mean;
+    return sum / total_weights;
 }
 
 int main(){
-    double cf = cgpa(3,70,3,20,5,0,2);
+    int n_subj;
+    cout << "How many subjects do you have? ";
+    cin >> n_subj;
+
+    int* grades = (int*)malloc(sizeof(int)*n_subj);
+    int* weights = (int*)malloc(sizeof(int)*n_subj);
+
+    for (int i = 0; i < n_subj; i++) {
+        cout << "Grade " << i+1 << ":\t";
+        cin >> *(grades+i);
+        cout << "Weight " << i+1 << ":\t";
+        cin >> *(weights+i);
+        cout << endl;
+    }
+
+    double cf = cgpa(n_subj,grades,weights);
+    cout << "CGPA:\t\t" << cf << endl;
+
     return 0;
 }
