99660b405f220897c5043fd73610a6a2818b6367
moisescrn <moisescarbofuster@hotmail.com>
2026-05-18 22:34:15 +0200

    First day of 03; player class created

---
+++ 03_Casino_Number_Guessing_Game/Player.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "Player.h"
+
+// Constructor
+Player::Player(string name, float initMoney) {
+    playerName = name;
+    currentMoney = initMoney;
+    wins = 0;
+    losses = 0;
+    printf("The player %s has entered the casino with %.2f\n", playerName.c_str(), currentMoney);
+}
+
+void Player::displayStats() {
+    printf("Stats of %s:\n", playerName.c_str());
+    printf("Current money: %.2f€\n", currentMoney);
+    printf("%i wins of %i times played.\n", wins, (wins+losses));
+    return;
+}
---
+++ 03_Casino_Number_Guessing_Game/Player.h
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+#ifndef PLAYER_H
+#define PLAYER_H
+class Player {
+private:
+    string playerName;
+    float currentMoney;
+    int wins;
+    int losses;
+
+public:
+    // Constructor
+    Player(string name, float initMoney);
+
+
+    void addMoney(int amount) { currentMoney += amount; } 
+    void subtractMoney(int amount) { currentMoney -= amount; }
+
+    void updateResult(bool win) { (win) ? wins+=1 : losses+=1; }
+
+    void getName() { printf("%s\n", playerName.c_str()); }
+    float getBalance() { return currentMoney; }
+
+    void displayStats();
+};
+#endif /*PLAYER_H*/
